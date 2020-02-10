90e1585102fca9ac10c00189d0453ae513564995
Jake Scott <bemybanker@gmail.com>
2020-02-09 18:20:58 -0600

    initial files and nav

---
+++ src/components/storage.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Navbar from "./nav/navbar"
+
+const Storage = () => {
+    return(
+    <React.Fragment>
+        <Navbar />
+    </React.Fragment>
+    )
+}
+
+export default Storage
